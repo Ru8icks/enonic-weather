929d75089e4559281198c8377344f22aa3f9ab29
Ru8ikcs <rjhansen@stud.ntnu.no>
2018-09-20 17:22:51 +0200

    styled

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@
  
 const url="http://api.openweathermap.org/data/2.5/group?id=3143244,2643743,625144&APPID=12cc04f0b185a80f366dba4548b294e7";
 
-const flex = document.getElementsByClassName("flex-container");
+
 // Create a request variable and assign a new XMLHttpRequest object to it.
 const request = new XMLHttpRequest();
 
@@ -12,57 +12,39 @@ request.open('GET', url, true);
 request.send();
 
 request.onload = function () {
-  // Begin accessing JSON data here
-  console.log(this.response)
-  console.log("haio")
-  var data = JSON.parse(this.response);
-  console.log(data)
-
-
+  let data = JSON.parse(this.response);
   let cities = document.getElementsByClassName("city");
   for (let i = 0; i < cities.length; i++){
       updateCity( cities[i], data.list[i])
-      console.log(data.list[i])
-
-
   }
   document.getElementById("update").innerHTML = "Last update @ "+ moment().format("LLLL");
-  
- 
-  
 }
 
-function updateCity(cityDiv, cityData){
-    console.log(cityData.name)
+
+function updateCity(cityDiv, cityData){    
     cityDiv.querySelector('#name').innerHTML=cityData.name;
     cityDiv.querySelector('#temp').innerHTML= Math.round(cityData.main.temp-273.15)+'&#8451;';
-    cityDiv.querySelector('#description').innerHTML=cityData.weather[0].description +  '<img src="assets/icons/'+cityData.weather[0].icon+ '.png">';
+    cityDiv.querySelector('#description').innerHTML=capFirstLetter(cityData.weather[0].description) +  '<img src="assets/icons/'+cityData.weather[0].icon+ '.png">';
     cityDiv.querySelector('#wind').innerHTML=cityData.wind.speed+" m/s, ("+ cityData.wind.deg+"&ordm;)";
     cityDiv.querySelector('#humidity').innerHTML=cityData.main.humidity+" %";
     cityDiv.querySelector('#sunrise').innerHTML= moment(cityData.sys.sunrise*1000).format("hh:mm a");
-    cityDiv.querySelector('#sunset').innerHTML= moment(cityData.sys.sunset*1000).format("hh:mm a");
-    
-    
-}
-
-function degToDir(num){
-
+    cityDiv.querySelector('#sunset').innerHTML= moment(cityData.sys.sunset*1000).format("hh:mm a");  
 }
 
 
 
+function capFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.substr(1);
+
+}
 
 
-
-// Send request
+//Update every minute
 setInterval(function(){  
     request.open('GET', url, true);    
     request.send();
-    
-
-
     }, 60000);
 
 
-//london: 2643743, oslo: 3143244, minsk: 625144
+
 
